c7ecaacb28f315ff5f2a91a440ea80a472d52ddd
Anna Marchenay <anna.marchenay@appsolute.fr>
2024-03-26 10:22:51 +0100

    Add placeholders for images

--- core/ui/src/main/java/com/marchenaya/core/ui/component/RecipeBookCard.kt
+++ core/ui/src/main/java/com/marchenaya/core/ui/component/RecipeBookCard.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,7 +33,8 @@ fun RecipeBookCard(cardImage: String, cardText: String, onClick: () -> Unit) {
                 model = cardImage,
                 contentDescription = cardText,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier.fillMaxSize(),
+                error = painterResource(id = R.drawable.ic_placeholder_24dp)
             )
         }
         RecipeBookText(
